d78e32dd27afed0e7de6369edf96ba39dc02b510
赵志宏(zhaozhihong) <zhaozhihong@inspur.com>
2020-10-14 20:08:46 +0800

    first commit

---
+++ src/main/java/com/vueboot/boot/dao/UserDAO.java
@@ -0,0 +1,10 @@
+package com.vueboot.boot.dao;
+
+import com.vueboot.boot.pojo.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserDAO extends JpaRepository<User,Integer> {
+    User findByUsername(String username);
+
+    User getByUsernameAndPassword(String username,String password);
+}
