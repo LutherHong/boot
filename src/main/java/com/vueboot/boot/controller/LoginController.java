c8a00ee8e315ef217fe302a141bcfec53ad863a5
赵志宏(zhaozhihong) <zhaozhihong@inspur.com>
2020-11-19 15:47:56 +0800

    chore: (十三) 使用 Shiro 实现用户信息加密与登录认证：添加注册页面

--- src/main/java/com/vueboot/boot/controller/LoginController.java
+++ src/main/java/com/vueboot/boot/controller/LoginController.java
@@ -2,7 +2,10 @@ package com.vueboot.boot.controller;
 
 import com.vueboot.boot.pojo.User;
 import com.vueboot.boot.result.Result;
+import com.vueboot.boot.result.ResultFactory;
 import com.vueboot.boot.service.UserService;
+import org.apache.shiro.crypto.SecureRandomNumberGenerator;
+import org.apache.shiro.crypto.hash.SimpleHash;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.util.HtmlUtils;
@@ -11,12 +14,12 @@ import javax.servlet.http.HttpSession;
 import java.util.Objects;
 
 @RestController
+@CrossOrigin
 public class LoginController {
 
     @Autowired
     UserService userService;
 
-    @CrossOrigin
     @PostMapping(value = "api/login")
     public Result login(@RequestBody User requestUser, HttpSession session) {
         // 对html标签进行逐一，放置XSS攻击
@@ -34,5 +37,33 @@ public class LoginController {
             session.setAttribute("user",user);
             return new Result(200);
         }
+    }
+
+    @PostMapping("api/register")
+    @ResponseBody
+    public Result register(@RequestBody User user) {
+        String username = user.getUsername();
+        String password = user.getPassword();
+        username = HtmlUtils.htmlEscape(username);
+        user.setUsername(username);
+
+        boolean exist = userService.isExist(username);
+        if (exist) {
+            String message = "用户名已被使用";
+            return ResultFactory.buildFailResult(message);
+        }
+
+        // 生成盐,默认长度 16 位
+        String salt = new SecureRandomNumberGenerator().nextBytes().toString();
+        // 设置 hash 算法迭代次数
+        int times = 2;
+        // 得到 hash 后的密码
+        String encodedPassword = new SimpleHash("md5", password, salt, times).toString();
+        // 存储用户信息，包括 salt 与 hash 后的密码
+        user.setSalt(salt);
+        user.setPassword(encodedPassword);
+        userService.add(user);
+
+        return ResultFactory.buildSuccessResult(user);
     }
 }
--- src/main/java/com/vueboot/boot/result/Result.java
+++ src/main/java/com/vueboot/boot/result/Result.java
@@ -1,19 +1,23 @@
 package com.vueboot.boot.result;
 
+import lombok.Data;
+
+@Data
 public class Result {
 
     //响应码
     private int code;
+    private String message;
+    private Object result;
 
     public Result(int code) {
         this.code = code;
     }
 
-    public int getCode() {
-        return code;
+    public Result(int code,String message,Object data){
+        this.code = code;
+        this.message = message;
+        this.result = data;
     }
 
-    public void setCode(int code) {
-        this.code = code;
-    }
 }
