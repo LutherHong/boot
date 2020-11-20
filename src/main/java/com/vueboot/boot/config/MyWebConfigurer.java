e533613f096b0fdd0c3f471a0735472f0dbed149
赵志宏(zhaozhihong) <zhaozhihong@inspur.com>
2020-11-20 19:46:45 +0800

    chore: (十四) 用户认证方案与完善的访问拦截：跨域，完成后端拦截

--- src/main/java/com/vueboot/boot/config/MyWebConfigurer.java
+++ src/main/java/com/vueboot/boot/config/MyWebConfigurer.java
@@ -4,6 +4,7 @@ import com.vueboot.boot.interceptor.LoginInterceptor;
 import org.springframework.boot.SpringBootConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -32,5 +33,15 @@ public class MyWebConfigurer implements WebMvcConfigurer {
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/api/file/**").addResourceLocations("file:" + "E:\\");
+    }
+
+    //为了允许跨域的 cookie
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowCredentials(true)     //在 allowCredentials(true) ，即允许跨域使用 cookie 的情况下
+                .allowedOrigins("http://localhost:8888")        // allowedOrigins() 不能使用通配符 *，这也是出于安全上的考虑
+                .allowedMethods("POST", "GET", "PUT", "OPTIONS", "DELETE")
+                .allowedHeaders("*");
     }
 }
--- src/main/java/com/vueboot/boot/interceptor/LoginInterceptor.java
+++ src/main/java/com/vueboot/boot/interceptor/LoginInterceptor.java
@@ -2,6 +2,10 @@ package com.vueboot.boot.interceptor;
 
 import com.vueboot.boot.pojo.User;
 import org.apache.commons.lang.StringUtils;
+import org.apache.shiro.SecurityUtils;
+import org.apache.shiro.subject.Subject;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.servlet.HandlerInterceptor;
 
 import javax.servlet.ServletContext;
@@ -11,27 +15,63 @@ import javax.servlet.http.HttpSession;
 
 public class LoginInterceptor implements HandlerInterceptor {
 
+//    /**
+//     * 拦截index开头的（此处专门拦截/index）拦截之后判断当前session中是否含有user，如果没有的话会重定向到/login
+//     * @param request
+//     * @param response
+//     * @param handler
+//     * @return
+//     * @throws Exception
+//     */
+//    @Override
+//    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+//        HttpSession session = request.getSession();
+//        String contextPath = session.getServletContext().getContextPath();
+//        String[] requireAuthPages  = new String[]{
+//                "index",
+//        };
+//        String uri = request.getRequestURI();
+//
+//        uri = StringUtils.remove(uri,contextPath+"/");
+//        String page = uri;
+//
+//        if(begingWith(page,requireAuthPages)){
+//            User user = (User) session.getAttribute("user");
+//            if (user==null){
+//                response.sendRedirect("login");
+//                return false;
+//            }
+//        }
+//        return true;
+//    }
+
+    /**
+     * 完善拦截访问
+     * 避免前端console里放一个假的用户。就直接进入用户了
+     * @param request
+     * @param response
+     * @param handler
+     * @return
+     * @throws Exception
+     */
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
-        HttpSession session = request.getSession();
-        String contextPath = session.getServletContext().getContextPath();
-        String[] requireAuthPages  = new String[]{
-                "index",
-        };
-        String uri = request.getRequestURI();
 
-        uri = StringUtils.remove(uri,contextPath+"/");
-        String page = uri;
+        // 放行 options 请求，否则无法让前端带上自定义的 header 信息，导致 sessionID 改变，shiro 验证失败
+        // 由于跨域情况下会先发出一个 options 请求试探，这个请求是不带 cookie 信息的，所以 shiro 无法获取到 sessionId，将导致认证失败。这个地方坑了我好几个小时，要不是文章早就写完了，我什么都配置好了，请求发过来 sessionId 还是老变。也怪我一直盯着后端，没仔细看前端发的是啥请求。
+        if (HttpMethod.OPTIONS.toString().equals(request.getMethod())) {
+            response.setStatus(HttpStatus.NO_CONTENT.value());
+            return true;
+        }
 
-        if(begingWith(page,requireAuthPages)){
-            User user = (User) session.getAttribute("user");
-            if (user==null){
-                response.sendRedirect("login");
-                return false;
-            }
+        Subject subject = SecurityUtils.getSubject();
+        // 使用 shiro 验证
+        if (!subject.isAuthenticated()) {
+            return false;
         }
         return true;
     }
+
 
     private boolean begingWith(String page, String[] requiredAuthPages) {
         boolean result = false;
